29877aa93a92e1ff41de404f498c2ae4d9cfbe78
orothy579 <108617889+orothy579@users.noreply.github.com>
2023-02-19 23:30:53 +0900

    hi

--- lib/screens/productPage.dart
+++ lib/screens/productPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:onebody/style/app_styles.dart';
 import 'package:onebody/style/bigText.dart';
+import 'package:onebody/style/icon_and_text_widget.dart';
 
 import '../style/smallText.dart';
 
@@ -14,11 +15,27 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> {
   PageController pageController = PageController(viewportFraction: 0.9);
+  var _currPageValue = 0.0;
+
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      _currPageValue= pageController.page!;
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
       height: 320,
       child: PageView.builder(
         controller: pageController,
@@ -29,12 +46,14 @@ class _ProductPageState extends State<ProductPage> {
     );
   }
   Widget _buildPageItem(int index){
+
+
     return Stack(
       children:
       [
         Container(
           height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
+          margin: EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
@@ -42,10 +61,16 @@ class _ProductPageState extends State<ProductPage> {
                   fit: BoxFit.cover,
                   image: NetworkImage(
                       "https://i.pinimg.com/564x/1a/fa/2c/1afa2cf5e0a145e4ceae527a14e99754.jpg"
-                  )
-
-
+                  ),
+              ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.7),
+                blurRadius: 5.0,
+                spreadRadius: 0.0,
+                offset: Offset(0,7)
               )
+            ]
           ),
         ),
 
@@ -53,11 +78,19 @@ class _ProductPageState extends State<ProductPage> {
           alignment: Alignment.bottomCenter,
           child:
           Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            height: 130,
+            margin: EdgeInsets.only(left: 40, right: 40, bottom: 30),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: Offset(0,7)
+                  )
+                ]
             ),
             child: Container(
               padding: EdgeInsets.only(top: 15, left: 15, right: 15),
@@ -70,17 +103,36 @@ class _ProductPageState extends State<ProductPage> {
                     children: [
                       Wrap(
                         children: List.generate(5, (index) {return Icon(Icons.star, color: mainGreen, size: 15,);}),
-                      )
+                      ),
+                      SizedBox(width: 10,),
+                      SmallText(text:"4.5"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "comments",)
                     ],
                   ),
-                  SizedBox(height: 10,),
-                  SmallText(text:"4.5"),
-                  SizedBox(height: 10,),
-                  SmallText(text: "1287"),
-                  SizedBox(height: 10,),
-                  SmallText(text: "comments",)
+                  SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: camel
+                      ),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7",
+                          iconColor: camel
+                      ),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_sharp,
+                          text: "32min",
+                          iconColor: camel
+                      ),
 
-
+                    ],
+                  )
 
 
                 ],
---
+++ lib/style/icon_and_text_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:onebody/style/smallText.dart';
+
+class IconAndTextWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconColor;
+  const IconAndTextWidget({Key? key,
+    required this.icon,
+    required this.text,
+    required this.iconColor}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+        Icon(icon, color: iconColor, ),
+        SizedBox(width: 5,),
+        SmallText(text: text ,)
+      ],
+    );
+  }
+}
+
+
