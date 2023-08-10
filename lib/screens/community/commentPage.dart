fec249699e0785d27f5756d4ca178b0515cf15d0
orothy579 <ftsky2050@naver.com>
2023-08-10 17:49:10 +0900

    commetPage

--- lib/screens/community/commentPage.dart
+++ lib/screens/community/commentPage.dart
@@ -5,45 +5,43 @@ import 'package:flutter/material.dart';
 
 import '../../model/Story.dart';
 
-class CommentPage extends StatefulWidget {
+class CommentPage extends StatelessWidget {
   final Story story;
+  final TextEditingController _commentController = TextEditingController();
 
   CommentPage({required this.story});
-
-  @override
-  _CommentPageState createState() => _CommentPageState();
-}
-
-class _CommentPageState extends State<CommentPage> {
-  final TextEditingController _commentController = TextEditingController();
-  final List<DocumentSnapshot> comments = [];
 
   Future<void> _submitComment() async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null && _commentController.text.isNotEmpty) {
-      final docRef = await FirebaseFirestore.instance
+      await FirebaseFirestore.instance
           .collection('story')
-          .doc(widget.story.id)
+          .doc(story.id)
           .collection('comments')
           .add({
         'userId': user.uid,
         'content': _commentController.text,
         'timestamp': Timestamp.now(),
       });
-
-      final doc = await docRef.get();
-
-      setState(() {
-        comments.insert(0, doc);
-        _commentController.clear();
-      });
+      _commentController.clear();
     }
   }
 
-  @override
-  void dispose() {
-    _commentController.dispose();
-    super.dispose();
+  String _timeAgoFromTimestamp(DateTime timestamp) {
+    final current = DateTime.now();
+    final difference = current.difference(timestamp);
+
+    if (difference.inSeconds < 60) {
+      return '${difference.inSeconds}초 전';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes}분 전';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours}시간 전';
+    } else if (difference.inDays < 30) {
+      return '${difference.inDays}일 전';
+    } else {
+      return '한 달 이상 전';
+    }
   }
 
   @override
@@ -82,7 +80,7 @@ class _CommentPageState extends State<CommentPage> {
               child: StreamBuilder(
                 stream: FirebaseFirestore.instance
                     .collection('story')
-                    .doc(widget.story.id)
+                    .doc(story.id)
                     .collection('comments')
                     .orderBy('timestamp', descending: true)
                     .snapshots(),
@@ -91,15 +89,37 @@ class _CommentPageState extends State<CommentPage> {
                     return CircularProgressIndicator();
                   }
 
-                  comments..clear()..addAll(snapshot.data!.docs);
-
+                  final comments = snapshot.data!.docs;
                   return ListView.builder(
                     itemCount: comments.length,
                     itemBuilder: (context, index) {
                       final doc = comments[index];
-                      return ListTile(
-                        title: Text(doc['content']),
-                        subtitle: Text(doc['timestamp'].toDate().toString()),
+                      return FutureBuilder<DocumentSnapshot>(
+                        future: FirebaseFirestore.instance.collection('users').doc(doc['userId']).get(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return CircularProgressIndicator();
+                          }
+
+                          final userDoc = snapshot.data!;
+                          final String username = userDoc['name'];
+                          final String userImage = userDoc['image'];
+
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(userImage),
+                            ),
+                            title: Text(doc['content']),
+                            subtitle: Row(
+                              children: [
+                                Text(username, style: TextStyle(fontWeight: FontWeight.bold)),
+                                SizedBox(width: 10), // A little spacing between username and time-ago string.
+                                Text(_timeAgoFromTimestamp(doc['timestamp'].toDate())),
+                              ],
+                            ),
+                          );
+
+                        },
                       );
                     },
                   );
@@ -122,9 +142,9 @@ class _CommentPageState extends State<CommentPage> {
           ],
         );
       },
-      initialChildSize: 1.0,
-      minChildSize: 0.5,
-      maxChildSize: 1.0,
+      initialChildSize: 1.0, // Change this value as needed
+      minChildSize: 0.5, // Change this value as needed
+      maxChildSize: 1.0, // Change this value as needed
     );
   }
 }
--- lib/screens/community/storyCard.dart
+++ lib/screens/community/storyCard.dart
@@ -3,22 +3,62 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart'; // 추가된 import
-
+import 'package:carousel_slider/carousel_slider.dart';
 import '../../model/Story.dart';
 import 'commentPage.dart';
 
+class StoryList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('story').orderBy('create_timestamp', descending: true).snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final storyDoc = snapshot.data!.docs[index] as QueryDocumentSnapshot<Map<String,dynamic>>;
+                  final story = Story.fromQuerySnapshot(storyDoc);
+                  return StoryCard(story: story);
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class StoryCard extends StatefulWidget {
   final Story story;
-
   StoryCard({required this.story});
-
   @override
   _StoryCardState createState() => _StoryCardState();
 }
 
-class _StoryCardState extends State<StoryCard> {
+class _StoryCardState extends State<StoryCard> with SingleTickerProviderStateMixin {
   bool _isLiked = false;
+  late AnimationController _heartAnimationController;
+  late Animation<double> _heartAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _heartAnimationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _heartAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+      parent: _heartAnimationController,
+      curve: Curves.elasticOut,
+    ));
+  }
 
   _toggleLike() async {
     final user = FirebaseAuth.instance.currentUser;
@@ -37,6 +77,11 @@ class _StoryCardState extends State<StoryCard> {
             .update({
           'likes': FieldValue.arrayUnion([user.uid]),
         });
+        _heartAnimationController.forward().then((_) {
+          Future.delayed(Duration(seconds: 1), () {
+            _heartAnimationController.reverse();
+          });
+        });
       }
       setState(() {
         _isLiked = !_isLiked;
@@ -49,7 +94,6 @@ class _StoryCardState extends State<StoryCard> {
   @override
   Widget build(BuildContext context) {
     _isLiked = widget.story.likes!.contains(FirebaseAuth.instance.currentUser!.uid);
-
     String displayDescription;
     if (widget.story.description!.length > 20) {
       displayDescription = _showFullDescription
@@ -58,142 +102,49 @@ class _StoryCardState extends State<StoryCard> {
     } else {
       displayDescription = widget.story.description!;
     }
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20.0),
-        border: Border.all(color: Colors.grey[200]!),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 2.0,
-            blurRadius: 5.0,
-            offset: Offset(0, 3),
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20.0),
+            border: Border.all(color: Colors.grey[200]!),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2.0,
+                blurRadius: 5.0,
+                offset: Offset(0, 3),
+              ),
+            ],
           ),
-        ],
-
-
-      ),
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // ... (기존의 Container 내용)
+            ],
+          ),
+        ),
+        Center(
+          child: ScaleTransition(
+            scale: _heartAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  radius: 25,
-                  backgroundImage: NetworkImage(widget.story.u_image!),
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.story.title!,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Text(
-                      widget.story.name!,
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ],
-                )
+                Icon(Icons.favorite, color: Colors.red, size: 200),
+                Text('공감해주셔서 감사해요!', style: TextStyle(fontWeight: FontWeight.bold)),
               ],
             ),
           ),
-          Container(
-            width: double.infinity,
-            height: 300.0,
-            child: CarouselSlider.builder(
-              itemCount: widget.story.images!.length,
-              itemBuilder: (context, index, realIdx) {
-                return Image.network(
-                  widget.story.images![index],
-                  fit: BoxFit.cover,
-                );
-              },
-              options: CarouselOptions(
-                autoPlay: false,
-                enlargeCenterPage: true,
-                viewportFraction: 1.0,
-                aspectRatio: 16/9,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        _isLiked ? Icons.favorite : Icons.favorite_border,
-                        color: _isLiked ? Colors.red : Colors.grey[600],
-                      ),
-                      onPressed: _toggleLike,
-                    ),
-                    Text('${widget.story.likes!.length}', style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-                IconButton(
-                  icon: Icon(Icons.comment, color: Colors.grey[600]),
-                  onPressed: () {
-                    showModalBottomSheet(
-                      context: context,
-                      isScrollControlled: true,
-                      builder: (BuildContext context) {
-                        double height = MediaQuery.of(context).size.height;
-
-                        return ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxHeight: height * 0.8,
-                          ),
-                          child: CommentPage(story: widget.story),
-                        );
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(20),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: widget.story.description!.length > 20
-                ? RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: [
-                  TextSpan(text: displayDescription),
-                  TextSpan(
-                    text: _showFullDescription ? " 접기" : " ... 더보기",
-                    style: TextStyle(color: Colors.blue),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        setState(() {
-                          _showFullDescription = !_showFullDescription;
-                        });
-                      },
-                  ),
-                ],
-              ),
-            )
-                : Text(widget.story.description!),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
 
-
+  @override
+  void dispose() {
+    _heartAnimationController.dispose();
+    super.dispose();
   }
 }
